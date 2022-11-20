a4dc5f415ab6b64622f7037dd3977d00f613c209
Mario Morais Neto <mariom_mmn@hotmail.com>
2022-11-20 15:45:46 -0300

    ajuste na classe Pedido

--- src/java/classes/Pedido.java
+++ src/java/classes/Pedido.java
@@ -3,7 +3,6 @@ package classes;
 import java.util.Date;
 
 public class Pedido {
-
     public enum Status {
         EM_ABERTO,
         REJEITADO,
@@ -14,8 +13,47 @@ public class Pedido {
         FINALIZADO
     }
     
-    int num_pedido;
-    Status status;
-    float orcamento;
-    Date prazo;
+    private Status status;
+    private float orcamento;
+    private Date prazo;
+    private int numPedido;
+    
+    public Pedido(Status status, float orcamento, Date prazo, int numPedido) {
+        this.status = status;
+        this.orcamento = orcamento;
+        this.prazo = prazo;
+        this.numPedido = numPedido;
+    }
+    
+    public int getNum_pedido() {
+        return numPedido;
+    }
+
+    public void setNum_pedido(int numPedido) {
+        this.numPedido = numPedido;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public float getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(float orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public Date getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(Date prazo) {
+        this.prazo = prazo;
+    }
 }
